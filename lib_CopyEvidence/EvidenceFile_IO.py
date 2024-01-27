03537c23e7f59a2ee2dd839e509133a271c9c487
ysuzuk81 <yuma1202@outlook.jp>
2024-01-27 16:23:21 +0900

    globを使用してパス名展開に対応　読み込んだパスをソートして保持するように変更

--- lib_CopyEvidence/EvidenceFile_IO.py
+++ lib_CopyEvidence/EvidenceFile_IO.py
@@ -1,24 +1,42 @@
+import glob
 import os
 
 import lib_CopyEvidence.File_IO as File_IO
 import lib_CopyEvidence.Path as Path
+from natsort import natsorted
 
 # エビデンスのパスを読み出して、エビデンスが存在するものとしないものを分けて返す
 def readEvidencePath(evidencePathFile):
     # エビデンスパスファイルからエビデンスパスを読み出す
     evidencePathList = File_IO.readLines(evidencePathFile)
-    # ディレクトリの区切り文字を'/'に変換
-    evidencePathList = [Path.convertPathDelimiterToSlash(evidencePath) for evidencePath in evidencePathList]
-    # パスの重複を削除
-    evidencePathList = list(set(evidencePathList))
 
     # 指定されたエビデンスパスのうち、存在するものとしないものを分ける
     existEvidencePathList = []
     notExistEvidencePathList = []
+
     for evidencePath in evidencePathList:
-        if os.path.exists(evidencePath):
-            existEvidencePathList.append(evidencePath)
+        # 指定されたエビデンスパスを検索
+        searchEvidencePathList = glob.glob(evidencePath)
+
+        # エビデンスが存在する場合
+        # パス名展開が行われた場合、存在するファイルのみ検索結果に入るはず
+        if not searchEvidencePathList == []:
+            existEvidencePathList.extend(searchEvidencePathList)
+
+        # エビデンスが存在しなかった場合
         else:
             notExistEvidencePathList.append(evidencePath)
 
+    # ディレクトリの区切り文字を'/'に変換
+    existEvidencePathList = [Path.convertPathDelimiterToSlash(evidencePath) for evidencePath in existEvidencePathList]
+    notExistEvidencePathList = [Path.convertPathDelimiterToSlash(evidencePath) for evidencePath in notExistEvidencePathList]
+
+    # パスの重複を削除
+    existEvidencePathList = list(set(existEvidencePathList))
+    notExistEvidencePathList = list(set(notExistEvidencePathList))
+
+    # ソート
+    existEvidencePathList = natsorted(existEvidencePathList)
+    notExistEvidencePathList = natsorted(notExistEvidencePathList)
+    
     return existEvidencePathList, notExistEvidencePathList
